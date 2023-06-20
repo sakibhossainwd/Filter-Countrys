b09151efb3692ace0374113ce170883e88f61574
sakibhossain <sakibhossainceo@gmail.com>
2023-06-19 21:27:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const loadCoutrys = (regionName) => {
+    const url = `https://restcountries.com/v3.1/region/${regionName}`
+    console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => regionCountrys(data))
+}
+
+const regionCountrys = countrys => {
+    const countryContainer = document.getElementById('country-container');
+    countryContainer.innerHTML = '';
+    countrys.forEach(country => {
+        // console.log(country.altSpellings[2]);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="col">
+                    <div class="card">
+                        <img src="${country.flags.png}" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Name: ${country.name.common}</h5>
+                          <p class="card-text">Spellings: ${country.altSpellings[2]}</p>
+                        </div>
+                    </div>
+        </div>
+        `
+        countryContainer.appendChild(div);
+    });
+}
+
+document.getElementById('region-list').addEventListener('click', function(even){
+    const regionName = even.target.innerText;
+    console.log(regionName);
+    loadCoutrys(regionName);
+    // const regionCountry = document.getElementById('regionAfrica');
+    // const regionAfrica = regionCountry.innerText;
+    // console.log(regionAfrica);
+    // loadCoutrys(regionAfrica);
+    
+})
+
+loadCoutrys('Asia');
